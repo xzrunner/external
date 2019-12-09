c29264a94513e10b8b5d3554910dbe719d3dc802
zz <xzrunner@gmail.com>
2019-12-10 07:29:03 +0800

    [ADDED] cgal

---
+++ cgal/Installation/include/CGAL/version.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2009
+// Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : -
+
+#ifndef CGAL_VERSION_H
+#define CGAL_VERSION_H
+
+#define CGAL_VERSION 5.1
+#define CGAL_VERSION_NR 1050100000
+#define CGAL_SVN_REVISION 99999
+#define CGAL_GIT_HASH abcdef
+#define CGAL_RELEASE_DATE 20191108
+
+#include <CGAL/version_macros.h>
+
+#endif
